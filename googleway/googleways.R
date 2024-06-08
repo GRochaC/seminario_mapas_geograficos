c34584cbc06a03e95e2ebaec2a1b295e8c69108e
gahenvi1 <gabriel.henrique.ovidio@gmail.com>
2024-06-08 16:42:31 -0300

    finalizando googleway

--- googleway/googleways.R
+++ googleway/googleways.R
@@ -1,6 +1,6 @@
-library(googleway)
-library(ggmap)
-library(ggplot2)
+if (!require("pacman")) install.packages("pacman")
+pacman::p_load("ggplot2", "ggmap", "googleway", "tidyverse")
+
 
 # chave
 key <- "AIzaSyC78zbXYMb-7nD8R-YiGruRRXicfsdhqiY"
@@ -33,7 +33,7 @@ rota <- decode_pl(direcao$routes$overview_polyline$points)
 
 mapa2<- get_map(location = c(lon = mean(rota$lon), lat = mean(rota$lat)), zoom = 15) 
 ggmap(mapa2) +
-  geom_path(data = rota, aes(x = lon, y = lat), color = "blue", size = 2)+
+  geom_path(data = rota, aes(x = lon, y = lat), color = "blue", size = 1)+
   geom_point(data = pontos, aes(x = lon, y = lat), color = "red", size = 5)
 
 
@@ -51,11 +51,16 @@ texto <- google_places(search_string = "Mercados na asa norte, brasilia, Brasil"
                       language = "pt-BR")
 
 # idiomas: https://developers.google.com/maps/faq#languagesupport
-resultados_texto <- as.df(texto[["results"]])
+resultados_texto <- texto[["results"]]
 
-mapa3<- get_map(location = c(lon = mean(resultados$geometry$location$lng), lat = mean(resultados$geometry$location$lat)), zoom = 14) 
+mapa3<- get_map(location = c(lon = mean(resultados_texto$geometry$location$lng), lat = mean(resultados_texto$geometry$location$lat)), zoom = 14) 
 ggmap(mapa3) +
-  geom_point(data = resultados, aes(x = geometry$location$lng, y = geometry$location$lat), color = "red", size = 4)
+  geom_point(data = resultados_texto, aes(x = geometry$location$lng, y = geometry$location$lat), color = "red", size = 4)+
+  theme_void()
+
+
+
+
 
 ## Por proximidade
 proximo<-  google_places(location = c(22.9068, 43.1729),
@@ -63,30 +68,30 @@ proximo<-  google_places(location = c(22.9068, 43.1729),
                          radius = 5000,
                          key = key,
                          language = "pt-BR")
-resultados_proximidade<- as.df(proximo[["results"]])
 
 
-#proximidade
 
-# Perform the proximity search
-places <- google_places(
+proximidade <- google_places(
   location = c(-15.808595, -47.885220),
   radius = 1000,
   keyword = "coffee shop",
   key = key
 )
 
-# Extract the results
-places_results <- places$results
+centro_lng <- mean(proximidade$results$geometry$location$lng)
+centro_lat <- mean(proximidade$results$geometry$location$lat)
+  
+  
+mapa4<- get_map(location = c(centro_lng, centro_lat), zoom = 14)
+
+ggmap(mapa4) +
+  geom_point(data = proximidade$results, aes(x = geometry$location$lng, y = geometry$location$lat), color = "red", size = 4)+
+  theme_void()
+
+
+google_map(key = key, search_box = T) %>%
+  add_traffic()
 
 
 
-# Prepare data for plotting
-places_df <- places_results %>%
-  select(name, geometry) %>%
-  mutate(lon = geometry$location$lng, lat = geometry$location$lat) %>%
-  select(name, lon, lat)
-
-# Get the map centered around the search location
-map <- get_map(location = c(-15.808595, -47.885220), zoom = 14)
 
